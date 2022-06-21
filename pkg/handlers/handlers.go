0a11b7e24d3bc44a20d92e44034feb891c109aca
GodKimba <rafaelfadel2@gmail.com>
2022-06-21 18:23:48 -0300

    separeted files and packages into their respective directories

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/godkimba/super-duper-golang-web-app/pkg/render"
+)
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.tmpl")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	RenderTemplate(w, "home.page.tmpl")
+}
